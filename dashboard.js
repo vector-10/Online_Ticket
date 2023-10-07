837a069c9a6265a8536a7575cd2d487c09c49a54
Vector-9 <duzieblaise10@gmail.com>
2023-10-07 08:47:51 +0100

    update

--- dashboard.js
+++ dashboard.js
@@ -1,45 +1,10 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".themeToggler");
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-  console.log("clicked");
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
-
-menuBtn.addEventListener("click", () => {
-  sideMenu.style.display = "block";
-});
-
-closeBtn.addEventListener("click", () => {
-  sideMenu.style.display = "none";
-});
 
 themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme-variables");
 
   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
-});
-
-Orders.forEach((order) => {
-  const tr = document.createElement("tr");
-  const trContent = `  <td>${order.ticketName}</td>
-                            <td${order.ticketNumber}</td>                           
-                            <td class="warning">${order.paymentStatus}</td>                        
-                            <td class="primary">Details</td>`;
-
-  tr.innerHTML = trContent;
-  document.querySelector("table tbody").appendChild(tr);
 });
